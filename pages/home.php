23e136e149295d50ab123024a01417a063a3bc19
Carollo <leocarollo@orange.fr>
2017-11-20 18:50:57 +0100

    Refonte des pages

--- pages/home.php
+++ pages/home.php
@@ -1,29 +1,39 @@
-     <div class="w-75 m-auto">
-	      	<div class="container">
-
-			<?php
-				$confs = getJSON("db/conf.json");
-
-				if($confs){
-				
-					foreach($confs as $conf){
-					echo '
-						<div class="meeting w-75 pt-5">
-							<div class="container row">
-								<div class="pr-4">'.$conf['datetime'].'</div>
-								<div class="col">
-									<h2>'.$conf['titre'].'</h2>
-									<p>'.$conf['lieu'].'</p>
-									<p>'.$conf['speaker'].'</p>
-								</div>
-								<div>'.$conf['description'].'</div>
-							</div>
-						</div>
-					';
-					}
-				}
-			?>
-
-	      	</div>
-	  	</div>
+<div id="blue">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-4">
+				<h3>Next Conferences </h3>
+			</div>
+		</div>
 	</div>
+</div>
+<div class="container mtb">
+	 	<div class="row">
+		<?php
+			$confs = getJSON("db/conf.json");
+			if($confs){
+				foreach($confs as $conf){
+				echo '
+					<div class="col-lg-8">
+						<h3> '.$conf['titre'].' - '.$conf['datetime'].'</h3>
+						<div> <i>'.$conf['lieu'].'</i></div>
+						<div "class="col">
+								<p> Conférencier: '.$conf['speaker'].'</p>
+						</div>
+						<div>'.$conf['description'].'</div>
+						<div class="spacing"></div>
+				 		<h6>SHARE :</h6>
+							<p class="share">
+		 					<a href="#"><i class="fa fa-twitter"></i></a>
+		 					<a href="#"><i class="fa fa-facebook"></i></a>
+		 					<a href="#"><i class="fa fa-google-plus"></i></a>		 		
+		 				</p>
+		 			<div class="hline"></div>
+			 		<div class="spacing"></div>
+					</div>
+				';
+				}
+			}
+		?>
+      	</div>
+</div>
--- pages/login.php
+++ pages/login.php
@@ -1,19 +1,33 @@
-	   <?php
-	   $text = NULL;
-	   	if(isset($_GET['callback']) && !$_GET['callback']){
-	   		$text = "Les données saisies ne sont pas valides";
-	   	}
-	   ?>
+<?php
+	$text = NULL;
+	if(isset($_GET['callback']) && !$_GET['callback']){
+			$text = "Les données saisies ne sont pas valides";
+	}
+?>
 
-	    <div class="w-50 m-auto">
-		  <p><?=$text?></p>
-	      	<form class="form-signin" action="" method="post">
+<div id="blue">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-3">
+				<h3>Connect to add conferences</h3>
+			</div>
+		</div><!-- /row -->
+	</div> <!-- /container -->
+</div><!-- /blue -->
+
+<div class="container mtb">
+	<p><?=$text?></p>
+	<div class="row">
+		<div class="col-lg-8">
+			<form  action="" method="post" role="form">
 				<h2 class="form-signin-heading">Sign in</h2>
 				<label for="login" class="sr-only">Login</label>
 				<input type="text" id="login" name="login" class="form-control" placeholder="Login" required autofocus>
 				<label for="pwd" class="sr-only">Password</label>
 				<input type="password" id="pwd" name="pwd" class="form-control" placeholder="Password" required>
-				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+				<div class="spacing"></div>
+				<button type="submit" class="btn btn-theme">Connect</button>
 			</form>
-	  	</div>
-	</div>
+		</div><! --/col-lg-8 -->
+	</div><! --/row -->
+</div><! --/container -->
